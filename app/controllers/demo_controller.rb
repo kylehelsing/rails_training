4f64e004b4c3150fafcfe5a64f088703860e5396
shelsing <helsing440@gmail.com>
2015-06-24 22:42:28 -0400

    Routes and Redirects

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,4 +2,13 @@ class DemoController < ApplicationController
   layout false
   def index
   end
+
+  def hello	
+  end
+  def other_hello
+  	redirect_to(:controller => 'demo', :action => 'index')
+  end	
+  def lynda
+  	redirect_to("http://lynda.com")
+  end
 end
